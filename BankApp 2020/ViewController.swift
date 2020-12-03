33e4c5e492f7b6fa68089a3f1ba15eb25cf09278
Denis Bystruev <dbystruev@me.com>
2020-12-03 15:53:26 +0300

    Initial Commit

---
+++ BankApp 2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp 2020
+//
+//  Created by Denis Bystruev on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
